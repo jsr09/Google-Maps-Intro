1071a3880437d0c8f321c8cf168431661dab175d
jsr09 <97914865+jsr09@users.noreply.github.com>
2023-04-28 07:01:55 -0400

    created sidebar navbar map components learning tailwind styling.

--- googlemapintro/src/App.js
+++ googlemapintro/src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import Map from "./components/Map";
 
 function App() {
   return (
-    <div className= 'text-xl text-center font-semibold bg-blue-500 text-white'>
-      Hello World!      
+    <div className="flex h-screen">
+      <div className="w-1/5">
+        <SideBar />
+      </div>
+      <div className="w-4/5">
+        <div className="mb-4">
+          <NavBar />
+        </div>
+        <div className="h-full">
+          <Map />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ googlemapintro/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="text-center bg-zinc-500 text-slate-400 height-">
+        Hello World!
+    </nav>
+  );
+};
+
+
+export default NavBar;
