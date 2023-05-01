d01b2402dfc7193eb548e9297600012ca70ad5c3
jsr09 <97914865+jsr09@users.noreply.github.com>
2023-05-01 02:07:36 -0400

    Down graded Main branch to react 17

--- googlemapintro/src/components/NavBar.jsx
+++ googlemapintro/src/components/NavBar.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center  bg-blue-500 text-white px-6 h-12">
-      <div class='text-left w-1/4 ml-0'>
+      <div className='text-left w-1/4 ml-0'>
         <Link to="/" className="mr-4 hover:text-sky-400">Home</Link>
       </div>
       <h2 className="w-1/2justify-items-center text-lg font-semibold">Your Day</h2>
-      <div class='text-right w-1/4'>
+      <div className='text-right w-1/4'>
         <Link to="/issues" className="mr-4 hover:text-sky-400">Issues</Link>
         <Link to="/futurefeatures"className="hover:text-sky-400">Future Features</Link>
       </div>
--- googlemapintro/src/index.js
+++ googlemapintro/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,13 +7,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
